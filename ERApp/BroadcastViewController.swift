2b1b269eaa7b709624b8bb6be9ef5ef346c2c72f
MARUYAMA GOHTA <maruyama.g.5@gmail.com>
2021-12-17 10:32:06 +0900

    BroadcastViewController編集中

--- ERApp/BroadcastViewController.swift
+++ ERApp/BroadcastViewController.swift
@@ -43,62 +43,6 @@ class BroadcastViewController: UIViewController, UITableViewDelegate, UITableVie
         //        searchBar.delegate = self
         //        searchBar.enablesReturnKeyAutomatically = false
         
-        
-        
-        //        if Auth.auth().currentUser != nil {
-        //            // listenerを登録して投稿データの更新を監視する
-        //            let areasRef = Firestore.firestore().collection("areas").order(by: "name")
-        //            areasRef.getDocuments() { (querySnapshot, error) in
-        //                if let error = error {
-        //                    print("DEBUG_PRINT: snapshotの取得が失敗しました。 \(error)")
-        //                    return
-        //                }
-        //                self.areasArray = querySnapshot!.documents.map { document in
-        //
-        //                    let area = AreaData(document: document)
-        //                    print("DEBUG_PRINT: document取得 \(area.name)")
-        //                    // ここでさらにhallsを取得する。
-        //                    let hallsRef = Firestore.firestore().collection("areas").document(area.id).collection("halls").order(by: "name")
-        //                    hallsRef.getDocuments() { (hallQuerySnapshot, hallError) in
-        //                        if let hallError = hallError {
-        //                            print("DEBUG_PRINT: snapshotの取得が失敗しました。 \(hallError)")
-        //                            return
-        //                        }
-        //                        area.halls = hallQuerySnapshot!.documents.map { document in
-        //
-        //                            let hall = HallData(document: document)
-        //                            print("DEBUG_PRINT: document取得 \(hall.name)")
-        //
-        //                            let broadcastsRef = Firestore.firestore().collection("areas").document(area.id).collection("halls").document(hall.id).collection("broadcasts").order(by: "name")
-        //                            broadcastsRef.getDocuments() {(broadcastQuerySnapshot, broadcastError) in
-        //                                if let broadcastError = broadcastError {
-        //                                    print("DEBUG_PRINT: snapshotの取得が失敗しました。\(broadcastError)")
-        //                                    return
-        //                                }
-        //                                hall.broadcasts = broadcastQuerySnapshot!.documents.map { document in
-        //
-        //                                    let broadcast = BroadcastData(document: document)
-        //                                    print("DEBUG_PRINT: document取得 \(broadcast.name)")
-        //
-        //                                    return broadcast
-        //                                }
-        //                                self.tableView.reloadData()
-        //                            }
-        //
-        //                            return hall
-        //                        }
-        ////                        self.tableView.reloadData()
-        //                    }
-        //                    return area
-        //
-        //                }
-        // TableViewの表示を更新する
-        //                self.tableView.reloadData()
-        
-        //            }
-        //
-        //
-        //        }
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
